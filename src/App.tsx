5b5d349f81cb487e43ce7e448be46c2b62b5b03d
thien.dang <thien.dang@ncc.asia>
2022-03-19 22:16:34 +0700

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { Refine } from "@pankod/refine-core";
+import { notificationProvider, LoginPage } from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import "styles/antd.less";
+import dataProvider from "@pankod/refine-simple-rest";
+import { authProvider } from "./authProvider";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      notificationProvider={notificationProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      authProvider={authProvider}
+      LoginPage={LoginPage}
+      resources={[
+        {
+          name: "posts",
+          list: PostList,
+          create: PostCreate,
+          edit: PostEdit,
+          show: PostShow,
+        },
+      ]}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+      i18nProvider={i18nProvider}
+    />
+  );
+}
+
+export default App;
