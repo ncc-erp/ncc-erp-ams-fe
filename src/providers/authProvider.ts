e0437dbdf96539276755934817107bcc094af979
Thuy Ngoc <ngoc.maithuy@ncc.asia>
2022-06-18 06:09:41 +0700

    edit task permissions

--- src/providers/authProvider.ts
+++ src/providers/authProvider.ts
@@ -2,7 +2,6 @@ import { AuthProvider } from "@pankod/refine-core";
 import dataProvider from "providers/dataProvider";
 import { UserAPI } from "api/userApi";
 import { GETME_API } from "api/baseApi";
-import { asyncLocalStorage } from "utils/asyncLocalStorage";
 
 export const TOKEN_KEY = "nhfi49hinsdjfnkaur8u3jshbd";
 export const STORE_PERMISSION = "permissions";
@@ -31,9 +30,9 @@ export const authProvider: AuthProvider = {
       url: url,
       payload: payload,
     });
-    await asyncLocalStorage.setItem(TOKEN_KEY, data.data.access_token);
+    localStorage.setItem(TOKEN_KEY, data.data.access_token);
     const permissionRes = await UserAPI.getAll(GETME_API);
-    await asyncLocalStorage.setItem(
+    localStorage.setItem(
       STORE_PERMISSION,
       JSON.stringify(permissionRes.data.permissions)
     );
