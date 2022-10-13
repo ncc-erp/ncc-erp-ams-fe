ef1e65bdf5996197921c460372533a8ee4eed30c
Hoai Thanh <thanh.maihoai@ncc.asia>
2022-10-13 08:31:36 +0700

    fix bug permission

--- src/untils/assets.ts
+++ src/untils/assets.ts
@@ -87,5 +87,21 @@ export const filterAssignedStatus = [
   },
 ]
 
+export function parseJwt (token: string | null): any{
+  if(!token) return;
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+  return JSON.parse(jsonPayload);
+}
+
 
 
