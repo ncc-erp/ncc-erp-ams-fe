64cea58e458e8a3e25b8a12314265a571bdc711b
phamphanbang <phamphanbang@gmail.com>
2023-06-22 12:11:12 +0700

    add taxtoken category

--- src/pages/categories/create.tsx
+++ src/pages/categories/create.tsx
@@ -143,6 +143,10 @@ export const CategoryCreate = (props: CategoriesCreateProps) => {
             {
               label: t("category.label.options.consumable"),
               value: "consumable",
+            },
+            {
+              label: t("category.label.options.taxtoken"),
+              value: "taxtoken",
             }
           ]}
         />
