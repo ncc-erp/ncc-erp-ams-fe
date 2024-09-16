1a0bcf23b58958dc83f3e32ecdbd0c88f11d39c7
huynguyenanh1 <huy.nguyenanh@ncc.asia>
2024-09-16 17:41:19 +0700

    fix bug review

--- src/pages/hardware/qr-control-panel.tsx
+++ src/pages/hardware/qr-control-panel.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { Select, Checkbox, Button } from "antd";
 import { useTranslation } from "react-i18next";
@@ -20,7 +19,7 @@ const QrControlPanel: React.FC<QrControlPanelProps> = ({
   const { t } = useTranslation();
   const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
-  const handleCheckboxChange = (e:CheckboxChangeEvent) => {
+  const handleCheckboxChange = (e: CheckboxChangeEvent) => {
     const checked = e.target.checked;
     setIsCheckboxChecked(checked);
     handleFieldChange("name");
@@ -30,21 +29,23 @@ const QrControlPanel: React.FC<QrControlPanelProps> = ({
   return (
     <div className="list__acction__qrcode">
       <div className="select__qrcode">
-        <div style={{ marginBottom: 16 }}>
-          <Select
-            value={layout}
-            onChange={(value) => setLayout(value as "above" | "below")}
-            style={{ width: "100%" }}
-            disabled={!isCheckboxChecked}
-          >
-            <Select.Option value="above">
-              {t("user.label.title.above")}
-            </Select.Option>
-            <Select.Option value="below">
-              {t("user.label.title.below")}
-            </Select.Option>
-          </Select>
-        </div>
+        {isCheckboxChecked && (
+          <div style={{ marginBottom: 16 }}>
+            <Select
+              value={layout}
+              onChange={(value) => setLayout(value as "above" | "below")}
+              style={{ width: "100%" }}
+              disabled={!isCheckboxChecked}
+            >
+              <Select.Option value="above">
+                {t("user.label.title.above")}
+              </Select.Option>
+              <Select.Option value="below">
+                {t("user.label.title.below")}
+              </Select.Option>
+            </Select>
+          </div>
+        )}
         <div style={{ marginBottom: 16 }}>
           <Checkbox onChange={handleCheckboxChange}>
             {t("user.label.title.codeDevice")}
