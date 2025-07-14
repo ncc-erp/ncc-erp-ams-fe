154ece0bb4ed4aa9d28a446a6b95f0f4d889072e
Nguyen Hue Suong <suongnhde180706@fpt.edu.vn>
2025-07-14 10:47:41 +0700

    feat(SUPPLIER): add column selection, advanced search, refresh and reset filter for supplier list

--- src/pages/supplier/list.tsx
+++ src/pages/supplier/list.tsx
@@ -1,4 +1,4 @@
-import { Image, Tooltip } from "antd";
+import { Badge, Checkbox, Image, Tooltip } from "antd";
 import {
   useTranslate,
   IResourceComponentsProps,
@@ -20,19 +20,48 @@ import {
 import { Spin } from "antd";
 
 import { TableAction } from "components/elements/tables/TableAction";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { MModal } from "components/Modal/MModal";
 import { ISupplier, ISupplierRequest } from "interfaces/supplier";
 import { SupplierCreate } from "./create";
 import { SupplierEdit } from "./edit";
 import { SUPPLIERS_API } from "api/baseApi";
+import { SupplierSearch } from "./search";
+import {
+  FileSearchOutlined,
+  SyncOutlined,
+  CloseOutlined,
+  FilterOutlined,
+} from "@ant-design/icons";
+import { MenuOutlined } from "@ant-design/icons";
+import { useSearchParams } from "react-router-dom";
+import { trimObjectValues } from "ultils/trimUtils";
 
+const defaultCheckedList = [
+  "name",
+  "image",
+  "address",
+  "assets_count",
+  "accessories_count",
+  "consumables_count",
+  "tools_count",
+  "digital_signatures_count",
+];
 export const SupplierList: React.FC<IResourceComponentsProps> = () => {
   const t = useTranslate();
+  const [isSearchModalVisible, setIsSearchModalVisible] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isEditModalVisible, setIsEditModalVisible] = useState(false);
   const [detail, setDetail] = useState<ISupplierRequest>();
-
+  const [isActive, setIsActive] = useState(false);
+  const [collumnSelected, setColumnSelected] = useState<string[]>(
+    localStorage.getItem("item_suppliers_selected") !== null
+      ? JSON.parse(localStorage.getItem("item_suppliers_selected") as string)
+      : defaultCheckedList
+  );
+  const onClickDropDown = () => setIsActive(!isActive);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
   const { tableProps, sorter, searchFormProps, tableQueryResult } =
     useTable<ISupplier>({
       initialSorter: [
@@ -44,12 +73,27 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
       resource: SUPPLIERS_API,
       onSearch: (params: any) => {
         const filters: CrudFilters = [];
-        const { search } = params;
-        filters.push({
-          field: "search",
-          operator: "eq",
-          value: search,
-        });
+        const trimmedParams = trimObjectValues(params);
+        const { search, name, address, phone, email, contact } = trimmedParams;
+
+        filters.push(
+          {
+            field: "search",
+            operator: "eq",
+            value: search,
+          },
+          {
+            field: "filter",
+            operator: "eq",
+            value: JSON.stringify({
+              name,
+              address,
+              phone,
+              email,
+              contact,
+            }),
+          }
+        );
         return filters;
       },
     });
@@ -86,6 +130,21 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
         title: t("supplier.label.field.address"),
         render: (value: ISupplier) => <TextField value={value} />,
       },
+      {
+        key: "phone",
+        title: t("supplier.label.field.phone"),
+        render: (value: ISupplier) => <TextField value={value} />,
+      },
+      {
+        key: "email",
+        title: t("supplier.label.field.email"),
+        render: (value: ISupplier) => <TextField value={value} />,
+      },
+      {
+        key: "contact",
+        title: t("supplier.label.field.contact"),
+        render: (value: ISupplier) => <TextField value={value} />,
+      },
 
       {
         key: "assets_count",
@@ -120,6 +179,48 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
     handleOpenModel();
   };
 
+  const handleOpenSearchModal = () => {
+    setIsSearchModalVisible(!isSearchModalVisible);
+  };
+  const [loading, setLoading] = useState(false);
+  const handleRefresh = () => {
+    setLoading(true);
+    setTimeout(() => {
+      refreshData();
+      setLoading(false);
+    }, 300);
+  };
+
+  const onCheckItem = (item: any) => {
+    const newCheckedList = [...collumnSelected];
+    if (newCheckedList.includes(item.key)) {
+      newCheckedList.splice(newCheckedList.indexOf(item.key), 1);
+    } else {
+      newCheckedList.push(item.key);
+    }
+    setColumnSelected(newCheckedList);
+    localStorage.setItem(
+      "item_suppliers_selected",
+      JSON.stringify(newCheckedList)
+    );
+  };
+
+  const handleResetFilter = () => {
+    localStorage.removeItem("search");
+
+    searchParams.delete("search");
+    setSearchParams(searchParams);
+
+    searchFormProps.form?.resetFields();
+    searchFormProps.form?.setFieldsValue({
+      search: undefined,
+    });
+
+    setTimeout(() => {
+      searchFormProps.form?.submit();
+    }, 0);
+  };
+
   const handleOpenModel = () => {
     setIsModalVisible(!isModalVisible);
   };
@@ -143,6 +244,13 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
   };
 
   useEffect(() => {
+    localStorage.setItem(
+      "item_suppliers_selected",
+      JSON.stringify(collumnSelected)
+    );
+  }, [collumnSelected]);
+
+  useEffect(() => {
     refreshData();
   }, [isEditModalVisible]);
 
@@ -161,7 +269,109 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
     >
       <div className="all">
         <TableAction searchFormProps={searchFormProps} />
+        <div className="other_function">
+          <div className="menu-container" ref={menuRef}>
+            <div>
+              <button
+                className="menu-trigger"
+                style={{
+                  borderTopLeftRadius: "3px",
+                  borderBottomLeftRadius: "3px",
+                }}
+              >
+                <Tooltip
+                  title={t("supplier.label.tooltip.refresh")}
+                  color={"#108ee9"}
+                >
+                  <SyncOutlined
+                    onClick={handleRefresh}
+                    style={{ color: "black" }}
+                  />
+                </Tooltip>
+              </button>
+            </div>
+            <div>
+              <button onClick={onClickDropDown} className="menu-trigger">
+                <Tooltip
+                  title={t("supplier.label.tooltip.columns")}
+                  color={"#108ee9"}
+                >
+                  <MenuOutlined style={{ color: "black" }} />
+                </Tooltip>
+              </button>
+            </div>
+            <nav className={`menu ${isActive ? "active" : "inactive"}`}>
+              <div className="menu-dropdown">
+                {collumns.map((item) => (
+                  <Checkbox
+                    className="checkbox"
+                    key={item.key}
+                    onChange={() => onCheckItem(item)}
+                    checked={collumnSelected.includes(item.key)}
+                  >
+                    {item.title}
+                  </Checkbox>
+                ))}
+              </div>
+            </nav>
+          </div>
+          <div>
+            <button
+              className="menu-trigger"
+              style={{
+                borderTopRightRadius: "3px",
+                borderBottomRightRadius: "3px",
+              }}
+            >
+              <Tooltip
+                title={t("supplier.label.tooltip.search_advanced")}
+                color={"#108ee9"}
+              >
+                <FileSearchOutlined
+                  onClick={handleOpenSearchModal}
+                  style={{ color: "black" }}
+                />
+              </Tooltip>
+            </button>
+          </div>
+          <div>
+            <button
+              className="menu-trigger"
+              onClick={() => handleResetFilter()}
+            >
+              <Tooltip
+                title={t("supplier.label.tooltip.resetFilter")}
+                color="#108ee9"
+              >
+                <Badge
+                  count={
+                    <CloseOutlined style={{ fontSize: 8, color: "white" }} />
+                  }
+                  size="small"
+                  offset={[-5, 5]}
+                  style={{
+                    backgroundColor: "#ff4d4f",
+                    boxShadow: "0 0 0 1px white",
+                  }}
+                >
+                  <FilterOutlined style={{ fontSize: 15, color: "black" }} />
+                </Badge>
+              </Tooltip>
+            </button>
+          </div>
+        </div>
       </div>
+      <MModal
+        title={t("supplier.label.title.search_advanced")}
+        setIsModalVisible={setIsSearchModalVisible}
+        isModalVisible={isSearchModalVisible}
+      >
+        <SupplierSearch
+          isModalVisible={isSearchModalVisible}
+          setIsModalVisible={setIsSearchModalVisible}
+          searchFormProps={searchFormProps}
+        />
+      </MModal>
       <MModal
         title={t("supplier.label.title.create")}
         setIsModalVisible={setIsModalVisible}
@@ -184,7 +394,7 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
         />
       </MModal>
 
-      {tableProps.loading ? (
+      {tableProps.loading || loading ? (
         <>
           <div style={{ paddingTop: "15rem", textAlign: "center" }}>
             <Spin
@@ -208,9 +418,11 @@ export const SupplierList: React.FC<IResourceComponentsProps> = () => {
               : false
           }
         >
-          {collumns.map((col) => (
-            <Table.Column dataIndex={col.key} {...col} key={col.key} sorter />
-          ))}
+          {collumns
+            .filter((collumn) => collumnSelected.includes(collumn.key))
+            .map((col) => (
+              <Table.Column dataIndex={col.key} {...col} key={col.key} sorter />
+            ))}
           <Table.Column<ISupplierRequest>
             title={t("table.actions")}
             dataIndex="actions"
