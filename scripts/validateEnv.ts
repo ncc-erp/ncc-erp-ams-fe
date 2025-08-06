e12b3065627f19e3d53df737a66a60b405c35d7c
thiennguyenhuuncc <thien.nguyenhuu@ncc.asia>
2025-08-06 09:56:40 +0700

    fix(AMS-132): using zod to validate the env

--- scripts/validateEnv.ts
+++ scripts/validateEnv.ts
@@ -1,37 +1,28 @@
-const fs = require("fs");
-const path = require("path");
+const { z } = require("zod");
+require("dotenv").config();
 
-const requiredEnvKeys = [
-  "REACT_APP_API_PROXY",
-  "REACT_APP_AUTH_CLIENT_ID",
-  "REACT_APP_SHOW_MANUAL_LOGIN",
-  "REACT_APP_GOOGLE_CLIENT_ID",
-];
+const envSchema = z.object({
+  REACT_APP_API_PROXY: z.string().nonempty("REACT_APP_API_PROXY is required"),
+  REACT_APP_AUTH_CLIENT_ID: z
+    .string()
+    .nonempty("REACT_APP_AUTH_CLIENT_ID is required"),
+  REACT_APP_SHOW_MANUAL_LOGIN: z
+    .string()
+    .nonempty("REACT_APP_SHOW_MANUAL_LOGIN is required"),
+  REACT_APP_GOOGLE_CLIENT_ID: z
+    .string()
+    .nonempty("REACT_APP_GOOGLE_CLIENT_ID is required"),
+});
 
-const envFilePath = path.resolve(__dirname, "../.env");
-// const envExampleFilePath = path.resolve(__dirname, "../.env.example");
+function validateEnv() {
+  const parsedEnv = envSchema.safeParse(process.env);
 
-function validateEnv(filePath, requiredKeys) {
-  if (!fs.existsSync(filePath)) {
-    console.error(`File not found: ${filePath}`);
+  if (!parsedEnv.success) {
+    console.error("Environment validation failed:", parsedEnv.error.format());
     process.exit(1);
   }
 
-  const envContent = fs.readFileSync(filePath, "utf-8");
-  const envKeys = envContent
-    .split("\n")
-    .filter((line) => line && !line.startsWith("#"))
-    .map((line) => line.split("=")[0]);
-
-  const missingKeys = requiredKeys.filter((key) => !envKeys.includes(key));
-
-  if (missingKeys.length > 0) {
-    console.error(`Missing required keys in ${filePath}:`, missingKeys);
-    process.exit(1);
-  }
-
-  console.log(`Validation passed for ${filePath}`);
+  console.log("Environment validation passed.");
 }
 
-validateEnv(envFilePath, requiredEnvKeys);
-// validateEnv(envExampleFilePath, requiredEnvKeys);
+validateEnv();
