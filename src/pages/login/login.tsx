17ffc79b1717459573502e2508c042ad4313193b
Tu Dang Van Hoai <tu.dangvanhoai@ncc.asia>
2025-08-11 11:57:15 +0700

    feat(AMS-128): set up key suggestion for i18n (#139)
    * feat(128): set up key suggestion for i18n
    
    * feat(128): update en language file
    
    * feat(128): fix default language
    
    * feat(AMS-128): fix lint

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from "react";
+import { useLogin } from "@pankod/refine-core";
 import {
-  Row,
-  Col,
-  Layout,
+  Button,
   Card,
-  Typography,
+  Checkbox,
+  Col,
   Form,
   Input,
-  Button,
-  Checkbox,
+  Layout,
+  Row,
+  Typography,
 } from "antd";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-import { useLogin, useTranslate } from "@pankod/refine-core";
-import { gapi } from "gapi-script";
-
-import {
-  layoutStyles,
-  containerStyles,
-  titleStyles,
-  imageContainer,
-  logo,
-  buttonLoginGoogle,
-} from "./styles";
-import "styles/antd.less";
-
-import dataProvider from "providers/dataProvider";
 import useLoginWithMezon from "hooks/useLoginWithMezon";
 import { useMezonLoginByHash } from "hooks/useMezonLoginByHash";
+import dataProvider from "providers/dataProvider";
+import "styles/antd.less";
+import {
+  buttonLoginGoogle,
+  containerStyles,
+  imageContainer,
+  layoutStyles,
+  logo,
+  titleStyles,
+} from "./styles";
 
 const { Title } = Typography;
 
@@ -43,7 +41,7 @@ export interface ILoginForm {
  */
 export const LoginPage: React.FC = () => {
   const [form] = Form.useForm<ILoginForm>();
-  const translate = useTranslate();
+  const { t } = useTranslation();
   const [isLoadingMezon, setIsLoadingMezon] = useState<boolean>(false);
 
   useLoginWithMezon();
@@ -53,7 +51,7 @@ export const LoginPage: React.FC = () => {
 
   const CardTitle = (
     <Title level={3} style={titleStyles} data-test-id="title">
-      {translate("pages.login.title", "Sign in your account")}
+      {t("pages.login.title")}
     </Title>
   );
 
@@ -108,21 +106,18 @@ export const LoginPage: React.FC = () => {
                   <Form.Item
                     name="username"
                     data-test-id="username"
-                    label={translate("pages.login.username", "Username")}
+                    label={t("pages.login.username")}
                     rules={[{ required: true }]}
                   >
                     <Input
                       size="large"
-                      placeholder={translate(
-                        "pages.login.username",
-                        "Username"
-                      )}
+                      placeholder={t("pages.login.username")}
                     />
                   </Form.Item>
                   <Form.Item
                     data-test-id="password"
                     name="password"
-                    label={translate("pages.login.password", "Password")}
+                    label={t("pages.login.password")}
                     rules={[{ required: true }]}
                     style={{ marginBottom: "12px" }}
                   >
@@ -144,7 +139,7 @@ export const LoginPage: React.FC = () => {
                           fontSize: "12px",
                         }}
                       >
-                        {translate("pages.login.remember", "Remember me")}
+                        {t("pages.login.remember")}
                       </Checkbox>
                     </Form.Item>
                   </div>
@@ -156,7 +151,7 @@ export const LoginPage: React.FC = () => {
                     loading={isLoading}
                     block
                   >
-                    {translate("pages.login.signin", "Sign in")}
+                    {t("pages.login.signin")}
                   </Button>
                 </Form>
               )}
@@ -177,7 +172,7 @@ export const LoginPage: React.FC = () => {
                 style={buttonLoginGoogle}
                 className="btn-login-mezon"
               >
-                {translate("pages.login.signinMezon", "Sign in with Mezon")}
+                {t("pages.login.signinMezon")}
               </Button>
             </Card>
           </div>
